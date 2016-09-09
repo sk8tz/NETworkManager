e4147c4c72f48fa2b131cdd27fa4145c6a4c70fc
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2016-09-09 04:53:55 +0200

    Localization added

---
+++ NETworkManager/NETworkManager/Core/Localization/LocalizationController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace NETworkManager.Core.Localization
+{
+    public static class LocalizationController
+    {
+        // List of all available localizations
+        public static List<LocalizationInfo> GetLocalizationList
+        {
+            get
+            {
+                return new List<LocalizationInfo> {
+                    new LocalizationInfo("English", "/Resources/Localization/Resources.en-US.xaml", new Uri("/Resources/Localization/Flags/en-US.png", UriKind.Relative), "BornToBeRoot", "en-US"),
+                    new LocalizationInfo("Deutsch", "/Resources/Localization/Resources.de-DE.xaml", new Uri("/Resources/Localization/Flags/de-DE.png", UriKind.Relative), "BornToBeRoot", "de-DE")
+                };
+            }
+        }
+        
+        // Current localization
+        private static LocalizationInfo _currentLocalization;
+        public static LocalizationInfo CurrentLocalization
+        {
+            get { return _currentLocalization; }
+            set { _currentLocalization = value; }
+        }
+
+        // Load localization from user settings, alternative use system language and if it's not available choose english
+        private static ResourceDictionary lastLocalization;
+
+        public static void LoadLocalization()
+        {
+            string cultureCode = Properties.Settings.Default.Localization_CultureCode;
+                        
+            if (string.IsNullOrEmpty(cultureCode))
+                cultureCode = CultureInfo.CurrentCulture.Name;
+
+            LocalizationInfo localizationInfo = GetLocalizationList.Where(x => x.Code == cultureCode).FirstOrDefault();
+
+            if (localizationInfo == null)
+                localizationInfo = GetLocalizationList.First();
+
+            if (lastLocalization != null)
+                Application.Current.Resources.MergedDictionaries.Remove(lastLocalization);
+
+            lastLocalization = new ResourceDictionary { Source = new Uri(localizationInfo.Path, UriKind.Relative) };
+
+            CurrentLocalization = localizationInfo;
+
+            Application.Current.Resources.MergedDictionaries.Add(lastLocalization);
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(CurrentLocalization.Code);
+        }
+    }
+}
--- NETworkManager/NETworkManager/MainWindow.xaml.cs
+++ NETworkManager/NETworkManager/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using NETworkManager.GUI;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +22,44 @@ namespace NETworkManager
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region RightWindowCommands
+        private async void btnGithub_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(Properties.Resources.GitHubProjectURL);
+            }
+            catch (Exception ex)
+            {
+                MetroDialogSettings dialogSettings = new MetroDialogSettings()
+                {
+                    AffirmativeButtonText = "OK",
+                    AnimateShow = true,
+                    AnimateHide = false
+                };
+
+                string dialogMessage = string.Format("Fehler beim öffnen der Github Project Seite.\n\nException:\n{0}", ex.Message);
+
+                await this.ShowMessageAsync("Error", dialogMessage, MessageDialogStyle.Affirmative, dialogSettings);
+            }
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings window = new Settings()
+            {
+                Owner = this
+            };
+
+            window.ShowDialog();
+        }
+        #endregion
     }
 }
