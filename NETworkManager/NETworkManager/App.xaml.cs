99e21f2c6ce90e533f70c056667daf47f954250a
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2016-10-26 21:05:40 +0200

    Application shutdown when MainWindow is closed

--- NETworkManager/NETworkManager/App.xaml.cs
+++ NETworkManager/NETworkManager/App.xaml.cs
@@ -7,5 +7,9 @@ namespace NETworkManager
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            ShutdownMode = ShutdownMode.OnLastWindowClose;
+        }
     }
 }
