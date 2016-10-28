d4847b0bc78f14b9411aebd54e89cd3c58b48e34
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2016-10-29 01:21:46 +0200

    Changed IPv4-Address validation

--- NETworkManager/NETworkManager/GUI/Validator/ValidateIPv4Address.cs
+++ NETworkManager/NETworkManager/GUI/Validator/ValidateIPv4Address.cs
@@ -1,5 +1,5 @@
 ﻿using System.Globalization;
-using System.Net;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,18 +9,8 @@ namespace NETworkManager.GUI.Validator
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            IPAddress ipAddr;
-
-            if (IPAddress.TryParse(value as string, out ipAddr))
-            {
-                switch (ipAddr.AddressFamily)
-                {
-                    case System.Net.Sockets.AddressFamily.InterNetwork:
-                        return ValidationResult.ValidResult;
-                    case System.Net.Sockets.AddressFamily.InterNetworkV6:
-                        return new ValidationResult(false, Application.Current.Resources["LocalizedString_ValidateError_IPv6NotSupported"] as string);
-                }
-            }
+            if (Regex.IsMatch(value as string, @"\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b"))
+                return ValidationResult.ValidResult;
 
             return new ValidationResult(false, Application.Current.Resources["LocalizedString_ValidateError_EnterValidIPv4Address"] as string);
         }
