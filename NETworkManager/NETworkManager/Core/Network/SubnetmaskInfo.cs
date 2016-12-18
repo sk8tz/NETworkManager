7eafdab6b7e9757a9eec91126e9cdeb5b51f691d
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2016-12-18 05:10:48 +0100

    Some changes

---
+++ NETworkManager/NETworkManager/Core/Network/SubnetmaskInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace NETworkManager.Core.Network
+{
+    public class SubnetmaskInfo
+    {
+        public int Suffix { get; set; }
+        public string Subnetmask { get; set; }
+        public int Hosts { get; set; }        
+
+        public SubnetmaskInfo()
+        {
+
+        }
+
+        public SubnetmaskInfo(int suffix, string subnetmask, int hosts)
+        {
+            Suffix = suffix;
+            Subnetmask = subnetmask;
+            Hosts = hosts;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} (/{1})", Subnetmask, Suffix);
+        }
+    }
+}
